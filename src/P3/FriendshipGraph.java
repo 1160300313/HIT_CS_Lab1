41acfa1c7b4b21a36c6842ec0e5f4a9f5bd0782c
1160300313 <1778103694@qq.com>
2018-03-03 16:24:09 +0800

    P3 Finished

--- src/P3/FriendshipGraph.java
+++ src/P3/FriendshipGraph.java
@@ -4,13 +4,25 @@ import java.util.*;
 import java.util.ArrayList;
 
 public class FriendshipGraph {
-	private static int[][] graph = new int[999][999];
-	private static int members;
+	public final int inf = 9999;
+	private int[][] graph = new int[inf][inf];
+	private int members;
 	{
 		members = 0;
+		for (int i = 0; i < inf; i++)
+			for (int j = 0; j < inf; j++)
+				graph[i][j] = inf;
 	}
 
-	private static List<String> personList = new ArrayList<>();
+	public int getMembers() {
+		return this.members;
+	}
+
+	public int[][] getGraph() {
+		return this.graph;
+	}
+
+	private List<String> personList = new ArrayList<>();
 
 	/**
 	 * Add a person into the person list.
@@ -19,7 +31,7 @@ public class FriendshipGraph {
 	 *            the name of the person
 	 *
 	 */
-	public static void addVertex(Person person) {
+	public void addVertex(Person person) {
 		String name = person.getName();
 		for (String temp : personList) {
 			if (temp.equals(name)) {
@@ -28,9 +40,9 @@ public class FriendshipGraph {
 			}
 		}
 		personList.add(name);
-		System.out.println(personList.get(members));
-		System.out.println(personList.size());
-		System.out.println(personList.indexOf(name));
+		// System.out.println(personList.get(members));
+		// System.out.println(personList.size());
+		// System.out.println(personList.indexOf(name));
 		members++;
 	}
 
@@ -39,31 +51,97 @@ public class FriendshipGraph {
 	 * @param srcPerson
 	 * @param dstPerson
 	 */
-	public static void addEdge(Person srcPerson, Person dstPerson) {
+	public void addEdge(Person srcPerson, Person dstPerson) {
 		int findex = personList.indexOf(srcPerson.getName());
-		System.out.println(findex);
+		// System.out.println(findex);
 		int sindex = personList.indexOf(dstPerson.getName());
-		System.out.println(sindex);
+		// System.out.println(sindex);
 		graph[findex][sindex] = 1;
 	}
 
 	/**
+	 * Use Dijkstra бнбн
 	 * 
 	 * @param srcPerson
 	 * @param dstPerson
 	 * @return the distance between two persons x (an integer which above 0) if
 	 *         they're connected, otherwise return -1
 	 */
-	public static int getDistance(Person srcPerson, Person dstPerson) {
-		return -1;
+	public int getDistance(Person srcPerson, Person dstPerson) {
+		int srcIndex = personList.indexOf(srcPerson.getName());
+		int dstIndex = personList.indexOf(dstPerson.getName());
+		if (srcIndex == -1 || dstIndex == -1) {// Both persons do not exist
+			System.out.println("No such person(s)!");
+			return -1;
+		}
+		if (srcIndex == dstIndex)
+			return 0;
+
+		// Dijkstra
+		int i, j, k;
+		int[] usedVertex = new int[inf];// 1:not used 0:used
+		for (i = 0; i < members; i++)
+			usedVertex[i] = 1;
+		usedVertex[srcIndex] = 0;
+		int[] distances = new int[inf];// array stored all distances
+		for (i = 0; i < members; i++)
+			distances[i] = graph[srcIndex][i];
+		for (i = 0; i < members - 1; i++) {
+			int[] via = new int[inf];
+			for (j = 0, k = 0; j < members; j++) {
+				if (usedVertex[j] == 1) {
+					via[k] = j;
+					k++;
+				}
+			}
+			int minDistance = inf;
+			int temp = srcIndex;// Temporary variable
+			for (j = 0; j < k; j++) {
+				int nowDistance = distances[via[j]];
+				if (nowDistance < minDistance) {
+					minDistance = nowDistance;
+					temp = via[j];
+				}
+			}
+			usedVertex[temp] = 0;
+			for (j = 0; j < members; j++) {
+				if (usedVertex[j] == 1)
+					distances[j] = Math.min(distances[j], distances[temp] + graph[temp][j]);
+			}
+		}
+		/*
+		 * for (i = 0; i < members; i++) System.out.print(distances[i] + "\t");
+		 * System.out.println();
+		 */
+		int distance = distances[dstIndex];
+		if (distance == inf)
+			return -1;
+		return distance;
 	}
 
 	public static void main(String[] args) {
-		FriendshipGraph g = new FriendshipGraph();
+		FriendshipGraph graph = new FriendshipGraph();
 		Person rachel = new Person("Rachel");
 		Person ross = new Person("Ross");
-		g.addVertex(rachel);
-		g.addVertex(ross);
-		g.addEdge(ross, rachel);
+		Person ben = new Person("Ben");
+		Person kramer = new Person("Kramer");
+		graph.addVertex(rachel);
+		graph.addVertex(ross);
+		graph.addVertex(ben);
+		graph.addVertex(kramer);
+		graph.addEdge(rachel, ross);
+		graph.addEdge(ross, rachel);
+		graph.addEdge(ross, ben);
+		graph.addEdge(ben, ross);
+		/*
+		 * int i, j; int members = graph.getMembers(); int[][] g =
+		 * graph.getGraph(); for (i = 0; i < members; i++) { for (j = 0; j <
+		 * members; j++) System.out.print(g[i][j] + "\t"); System.out.println();
+		 * }
+		 */
+		System.out.println(graph.getDistance(rachel, ross));// should print 1
+		System.out.println(graph.getDistance(rachel, ben));// should print 2
+		System.out.println(graph.getDistance(rachel, rachel));// should print 0
+		System.out.println(graph.getDistance(rachel, kramer));// should print -1
 	}
 }
--- test/P3/FriendshipGraphTest.java
+++ test/P3/FriendshipGraphTest.java
@@ -1,6 +1,7 @@
 package P3;
 
 import org.junit.Test;
+import static org.junit.Assert.*;
 
 public class FriendshipGraphTest {
 	@Test
@@ -18,5 +19,14 @@ public class FriendshipGraphTest {
 		graph.addEdge(ross, rachel);
 		graph.addEdge(ross, ben);
 		graph.addEdge(ben, ross);
+		/*
+		 * int i, j; int members = graph.getMembers(); int[][] g =
+		 * graph.getGraph(); for (i = 0; i < members; i++) { for (j = 0; j <
+		 * members; j++) System.out.print(g[i][j]); System.out.println(); }
+		 */
+		assertEquals(1, graph.getDistance(rachel, ross), 0.001);
+		assertEquals(2, graph.getDistance(rachel, ben), 0.001);
+		assertEquals(0, graph.getDistance(rachel, rachel), 0.001);
+		assertEquals(-1, graph.getDistance(rachel, kramer), 0.001);
 	}
 }
