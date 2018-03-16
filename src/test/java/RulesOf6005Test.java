a2050663ca938b670f6e0f86c2eead0dffa9c5bf
1160300313 <1778103694@qq.com>
2018-03-16 15:14:35 +0800

    POM3

--- src/test/java/RulesOf6005Test.java
+++ src/test/java/RulesOf6005Test.java
@@ -21,6 +21,6 @@ public class RulesOf6005Test {
 //                RulesOf6005.mayUseCodeInAssignment(false, true, false, false, false));
 //        assertTrue("Expected true: self-written required code",
 //                RulesOf6005.mayUseCodeInAssignment(true, false, true, true, true));
-assretTrue(true);
+		assertTrue(true);
     }
 }
